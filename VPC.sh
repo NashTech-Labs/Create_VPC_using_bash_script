93ec4c78336c49d0db0f917f14203cdf12d4abce
aamir7knoldus <aamir.shahab@knoldus.com>
2022-09-27 13:18:34 +0530

    Added: VPC script

--- VPC.sh
+++ VPC.sh
@@ -11,27 +11,27 @@ echo "Creating VPC..."
 
 # creating vpc and storing response
 awsResponse=$(aws ec2 create-vpc \
-			--cidr-block "$VPC_CIDR_BLOCK" \
+            --cidr-block "$VPC_CIDR_BLOCK" \
             --region "$REGION" \
-			--output json)
+            --output json)
 
 # getting vpc_id
 vpc_id=$(echo -e "$awsResponse" | \
-		/usr/bin/jq '.Vpc.VpcId' | \
-		tr -d '"')
+        /usr/bin/jq '.Vpc.VpcId' | \
+        tr -d '"')
 sleep 2
 
 # tagging vpc
 echo "Tagging vpc..."
 aws ec2 create-tags \
-		--resources "$vpc_id" \
-		--tags Key=Name,Value="$VPC_TAG_NAME" \
+        --resources "$vpc_id" \
+        --tags Key=Name,Value="$VPC_TAG_NAME" \
         --region "$REGION"
 
 # describe vpc_id
 echo "VPC description:"
 vpc_descrition=$(aws ec2 describe-vpcs \
-        --vpc-ids "$vpc_id" \
-        --region "$REGION")
+                --vpc-ids "$vpc_id" \
+                --region "$REGION")
 
 echo $vpc_descrition | /usr/bin/jq
